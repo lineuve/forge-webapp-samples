d4982e9f2bf802e84bbf0c07918b764c7099a8a2
Greg Rashkevitch <rashkevitch@gmail.com>
2016-05-29 14:03:00 +0300

    Init endpoints in a simpler way

--- client/scripts/config.example.js
+++ client/scripts/config.example.js
@@ -1,5 +1,4 @@
 var CLIENT_ID = '<your client id>'; // Your app key
 var REDIRECT_URI = '<address of app\'s home>/plugins/login/login-callback.html';// A redirect URI for your application
 var ENV = '<forge environment>'; // dev / stg / prd
-var SERVER_2_LEGGED = '<server endpoint that returns a 2-legged token>';
-var SERVER_3_LEGGED = '<server endpoint that returns a 3-legged token>';
+var AUTH_SERVER_URL = '<server endpoint that returns implements oauth 2.0 tokens>'; // i.e. http://local.host:3000
