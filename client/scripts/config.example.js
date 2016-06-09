bf723b5965a20a31165ce55f0b759641b242070d
Greg Rashkevitch <rashkevitch@gmail.com>
2016-06-09 09:03:41 +0300

    Prod environment should be "prod"

--- client/scripts/config.example.js
+++ client/scripts/config.example.js
@@ -1,4 +1,4 @@
 var CLIENT_ID = '<your client id>'; // Your app key
 var REDIRECT_URI = '<address of app\'s home>/plugins/login/login-callback.html';// A redirect URI for your application
-var ENV = '<forge environment>'; // dev / stg / prd
+var ENV = '<forge environment>'; // dev / stg / prod
 var AUTH_SERVER_URL = '<server endpoint that returns implements oauth 2.0 tokens>'; // i.e. http://local.host:3000
